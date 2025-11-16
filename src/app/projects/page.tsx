50e9cb81443ab67c14a42aa7337148be9a439182
macmini62 <victorkangacha@gmail.com>
2025-11-16 23:16:05 +0300

    Added projects to the portfolio

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,27 +1,26 @@
-import { Metadata } from "next";
-import { ProjectGrid } from "@/components/projects/project-grid";
-import { projectsData } from "@/data/projects";
+import { Metadata } from 'next';
+import { ProjectGrid } from '@/components/projects/project-grid';
+import { projectsData } from '@/data/projects';
 
 export const metadata: Metadata = {
   title: "Projects - Victor Kang'acha",
   description:
-    "Explore my portfolio of projects in machine learning and AI, and web development.",
+    'Explore my portfolio of projects in machine learning and AI, and web development.',
 };
 
 export default function ProjectsPage() {
   return (
-    <div className="container py-12">
-      <div className="mx-auto max-w-[58rem] space-y-6">
-        <h1 className="font-heading text-4xl font-bold md:text-6xl">
+    <div className='container py-12'>
+      <div className='mx-auto max-w-[58rem] space-y-6'>
+        <h1 className='font-heading text-4xl font-bold md:text-6xl'>
           Projects
         </h1>
-        <p className="text-xl text-muted-foreground">
-          A collection of projects showcasing my work in machine learning and
-          AI, cloud architecture, and web development. As well as open source
-          contributions.
+        <p className='text-xl text-muted-foreground'>
+          A collection of projects showcasing my work in data science, machine
+          learning, AI, and web development.
         </p>
       </div>
-      <div className="mt-16">
+      <div className='mt-16'>
         <ProjectGrid projects={projectsData} />
       </div>
     </div>
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,23 +1,50 @@
-import { Project } from "@/types/project";
+import { Project } from '@/types/project';
 
 export const projectsData: Project[] = [
   {
     id: '1',
     title: 'Portfolio',
-    description: 'A modern portfolio website build using Nextjs and AWS',
-    technologies: [
-      'NextJs',
-      'React',
-      'TailwindCss',
-      'TypeScript',
-      'AWS'
-    ],
+    description: 'A modern portfolio website build using Nextjs',
+    technologies: ['NextJs', 'React', 'TailwindCss', 'TypeScript', 'AWS'],
     image: '/projects/nice.jpeg',
     category: 'Web Development',
     links: {
       github: 'https://github.com/macmini62/victorkangacha-platform-io',
-      live: 'https://victorkangacha-platform-io.vercel.app'
+      live: 'https://victorkangacha-platform-io.vercel.app',
     },
-    featured: true
-  }
-]
+    featured: true,
+  },
+  {
+    id: '2',
+    title: 'Uber Data EDA',
+    description:
+      'A visualization project that uncovers the different patterns and insights in the uber dataset',
+    technologies: ['Python', 'Seaborn', 'Numpy', 'Matplotlib'],
+    image: '/projects/uber.png',
+    category: 'Data Science',
+    links: {
+      live: 'https://www.kaggle.com/code/macmini62/uber-eda',
+    },
+    featured: true,
+  },
+  {
+    id: '3',
+    title: 'Global_Security_Threats EDA',
+    description:
+      'Uncovering patterns and trends on global cybersecurity threats in years between 2015 - 2024',
+    technologies: [
+      'Python',
+      'Seaborn',
+      'Numpy',
+      'Matplotlib',
+      'Kagglehub',
+      'Sklearn',
+    ],
+    image: '/projects/security.png',
+    category: 'Data Science',
+    links: {
+      live: 'https://www.kaggle.com/code/macmini62/global-security-threats-eda/notebook',
+    },
+    featured: true,
+  },
+];
